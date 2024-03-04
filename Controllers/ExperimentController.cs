10bde43f07daec86558d6f4bc4a93c99afbdb88f
Ірина Наконешнюк <nakonirina1996@gmail.com>
2024-03-04 12:05:25 +0200

    Add project files.

---
+++ Controllers/ExperimentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ABTestTracker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExperimentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
